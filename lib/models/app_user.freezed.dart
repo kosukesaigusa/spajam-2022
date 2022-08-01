a3581bdfce4edb6528b6b27ad9298129da13d7c2
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-08-01 10:55:31 +0900

    feat: define app user model

---
+++ lib/models/app_user.freezed.dart
@@ -0,0 +1,230 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'app_user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+AppUser _$AppUserFromJson(Map<String, dynamic> json) {
+  return _AppUser.fromJson(json);
+}
+
+/// @nodoc
+mixin _$AppUser {
+  String get name => throw _privateConstructorUsedError;
+  @unionTimestampConverter
+  UnionTimestamp get createdAt => throw _privateConstructorUsedError;
+  @alwaysUseServerTimestampUnionTimestampConverter
+  UnionTimestamp get updatedAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $AppUserCopyWith<AppUser> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AppUserCopyWith<$Res> {
+  factory $AppUserCopyWith(AppUser value, $Res Function(AppUser) then) =
+      _$AppUserCopyWithImpl<$Res>;
+  $Res call(
+      {String name,
+      @unionTimestampConverter
+          UnionTimestamp createdAt,
+      @alwaysUseServerTimestampUnionTimestampConverter
+          UnionTimestamp updatedAt});
+
+  $UnionTimestampCopyWith<$Res> get createdAt;
+  $UnionTimestampCopyWith<$Res> get updatedAt;
+}
+
+/// @nodoc
+class _$AppUserCopyWithImpl<$Res> implements $AppUserCopyWith<$Res> {
+  _$AppUserCopyWithImpl(this._value, this._then);
+
+  final AppUser _value;
+  // ignore: unused_field
+  final $Res Function(AppUser) _then;
+
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as UnionTimestamp,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as UnionTimestamp,
+    ));
+  }
+
+  @override
+  $UnionTimestampCopyWith<$Res> get createdAt {
+    return $UnionTimestampCopyWith<$Res>(_value.createdAt, (value) {
+      return _then(_value.copyWith(createdAt: value));
+    });
+  }
+
+  @override
+  $UnionTimestampCopyWith<$Res> get updatedAt {
+    return $UnionTimestampCopyWith<$Res>(_value.updatedAt, (value) {
+      return _then(_value.copyWith(updatedAt: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_AppUserCopyWith<$Res> implements $AppUserCopyWith<$Res> {
+  factory _$$_AppUserCopyWith(
+          _$_AppUser value, $Res Function(_$_AppUser) then) =
+      __$$_AppUserCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String name,
+      @unionTimestampConverter
+          UnionTimestamp createdAt,
+      @alwaysUseServerTimestampUnionTimestampConverter
+          UnionTimestamp updatedAt});
+
+  @override
+  $UnionTimestampCopyWith<$Res> get createdAt;
+  @override
+  $UnionTimestampCopyWith<$Res> get updatedAt;
+}
+
+/// @nodoc
+class __$$_AppUserCopyWithImpl<$Res> extends _$AppUserCopyWithImpl<$Res>
+    implements _$$_AppUserCopyWith<$Res> {
+  __$$_AppUserCopyWithImpl(_$_AppUser _value, $Res Function(_$_AppUser) _then)
+      : super(_value, (v) => _then(v as _$_AppUser));
+
+  @override
+  _$_AppUser get _value => super._value as _$_AppUser;
+
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(_$_AppUser(
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as UnionTimestamp,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as UnionTimestamp,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_AppUser extends _AppUser {
+  const _$_AppUser(
+      {this.name = '',
+      @unionTimestampConverter
+          this.createdAt = const UnionTimestamp.serverTimestamp(),
+      @alwaysUseServerTimestampUnionTimestampConverter
+          this.updatedAt = const UnionTimestamp.serverTimestamp()})
+      : super._();
+
+  factory _$_AppUser.fromJson(Map<String, dynamic> json) =>
+      _$$_AppUserFromJson(json);
+
+  @override
+  @JsonKey()
+  final String name;
+  @override
+  @JsonKey()
+  @unionTimestampConverter
+  final UnionTimestamp createdAt;
+  @override
+  @JsonKey()
+  @alwaysUseServerTimestampUnionTimestampConverter
+  final UnionTimestamp updatedAt;
+
+  @override
+  String toString() {
+    return 'AppUser(name: $name, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_AppUser &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.createdAt, createdAt) &&
+            const DeepCollectionEquality().equals(other.updatedAt, updatedAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(createdAt),
+      const DeepCollectionEquality().hash(updatedAt));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_AppUserCopyWith<_$_AppUser> get copyWith =>
+      __$$_AppUserCopyWithImpl<_$_AppUser>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_AppUserToJson(
+      this,
+    );
+  }
+}
+
+abstract class _AppUser extends AppUser {
+  const factory _AppUser(
+      {final String name,
+      @unionTimestampConverter
+          final UnionTimestamp createdAt,
+      @alwaysUseServerTimestampUnionTimestampConverter
+          final UnionTimestamp updatedAt}) = _$_AppUser;
+  const _AppUser._() : super._();
+
+  factory _AppUser.fromJson(Map<String, dynamic> json) = _$_AppUser.fromJson;
+
+  @override
+  String get name;
+  @override
+  @unionTimestampConverter
+  UnionTimestamp get createdAt;
+  @override
+  @alwaysUseServerTimestampUnionTimestampConverter
+  UnionTimestamp get updatedAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$_AppUserCopyWith<_$_AppUser> get copyWith =>
+      throw _privateConstructorUsedError;
+}
