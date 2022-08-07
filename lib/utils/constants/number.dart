850e48eec13b3efc867552a30b0fca6e92143662
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-08-07 12:19:09 +0900

    feat: add dio related configurations and utils

--- lib/utils/constants/number.dart
+++ lib/utils/constants/number.dart
@@ -3,3 +3,9 @@ const tabletWidthThreshold = 992;
 
 /// AlertDialog.content の横幅の最小値
 const double alertDialogContentMinWidth = 300;
+
+/// API通信タイムアウトまでの時間 (ms)
+const connectionTimeoutMilliSeconds = 100000;
+
+/// APIレスポンスタイムアウトまでの時間 (ms)
+const receiveTimeoutMilliSeconds = 100000;
