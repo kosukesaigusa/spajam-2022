883554a8edbf806d6d20641806e7cdeae26e558a
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-13 20:57:17 +0900

    feat: create GitHubPage

---
+++ lib/pages/github_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'github_issue_page.dart';
+import 'github_repos_page.dart';
+
+class GitHubPage extends HookConsumerWidget {
+  const GitHubPage({super.key});
+
+  static const path = '/github';
+  static const name = 'GitHubPage';
+  static const location = path;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabController = useTabController(initialLength: 2);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GitHub'),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: const <Widget>[
+            Tab(text: 'Repo Search'),
+            Tab(text: 'Create Issue'),
+          ],
+        ),
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: TabBarView(
+          controller: tabController,
+          children: const [
+            GitHubReposPage(),
+            GitHubIssuePage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
