982120d550e59b549772145e5b04db1aa5fba65b
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-14 10:37:57 +0900

    refactor: Padding でラップせずプロパティを使用

--- lib/pages/github_issue_page.dart
+++ lib/pages/github_issue_page.dart
@@ -34,93 +34,91 @@ class GitHubIssuePage extends HookConsumerWidget {
       ],
     );
 
-    return Padding(
+    return ListView(
       padding: const EdgeInsets.all(16),
-      child: ListView(
-        physics: const NeverScrollableScrollPhysics(),
-        children: [
-          const Gap(8),
-          Text(
-            'Create a new Issue on KosukeSaigusa/spajam-2022',
-            style: context.displaySmall,
-          ),
-          const Gap(8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                width: context.displaySize.width * 0.4,
-                child: ElevatedButton(
-                  onPressed: !titleFieldEmpty.value || !commentFieldEmpty.value
-                      ? () {
-                          titleController.clear();
-                          commentController.clear();
-                        }
-                      : null,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(32),
-                      ),
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        const Gap(8),
+        Text(
+          'Create a new Issue on KosukeSaigusa/spajam-2022',
+          style: context.displaySmall,
+        ),
+        const Gap(8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: context.displaySize.width * 0.4,
+              child: ElevatedButton(
+                onPressed: !titleFieldEmpty.value || !commentFieldEmpty.value
+                    ? () {
+                        titleController.clear();
+                        commentController.clear();
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
                     ),
                   ),
-                  child: const Text(
-                    'Cancel',
-                    style: TextStyle(fontSize: 16),
-                  ),
                 ),
-              ),
-              SizedBox(
-                width: context.displaySize.width * 0.4,
-                child: ElevatedButton(
-                  onPressed: !commentFieldEmpty.value && !titleFieldEmpty.value
-                      ? () {
-                          // TODO(shimizu-saffle): issueを作成する
-                        }
-                      : null,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green.shade500,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(32),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Gap(8),
-          TextFormField(
-            controller: titleController,
-            decoration: InputDecoration(
-              hintText: 'Title',
-              hintStyle: context.headlineMedium,
-              border: InputBorder.none,
-            ),
-          ),
-          SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints.expand(height: context.displaySize.height * 0.6),
-              child: TextFormField(
-                controller: commentController,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: 'Leave a comment',
-                  hintStyle: context.headlineSmall,
-                  border: InputBorder.none,
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
             ),
+            SizedBox(
+              width: context.displaySize.width * 0.4,
+              child: ElevatedButton(
+                onPressed: !commentFieldEmpty.value && !titleFieldEmpty.value
+                    ? () {
+                        // TODO(shimizu-saffle): issueを作成する
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green.shade500,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const Gap(8),
+        TextField(
+          controller: titleController,
+          decoration: InputDecoration(
+            hintText: 'Title',
+            hintStyle: context.headlineMedium,
+            border: InputBorder.none,
           ),
-        ],
-      ),
+        ),
+        SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.expand(height: context.displaySize.height * 0.6),
+            child: TextField(
+              controller: commentController,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                hintText: 'Leave a comment',
+                hintStyle: context.headlineSmall,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
