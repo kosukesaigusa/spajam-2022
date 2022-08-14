c32af974aad72cb4fdf730104d44f9ad26765684
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-15 00:37:59 +0900

    feat: IssueRepository, createIssue() を定義

---
+++ lib/repositories/api/issue_repository.dart
@@ -0,0 +1,33 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../utils/api/api_client.dart';
+import '../../utils/exceptions/base.dart';
+import '../../utils/logger.dart';
+
+final issueRepositoryProvider = Provider.autoDispose(
+  (ref) => IssueRepository(ref.read(apiClientProvider)),
+);
+
+class IssueRepository {
+  IssueRepository(this._client);
+  final ApiClient _client;
+
+  /// POST repos/KosukeSaigusa/spajam-2022/issues API をコールして、
+  /// リポジトリに Issue を作成する。
+  Future<void> createIssue({
+    required String title,
+    required String body,
+  }) async {
+    final responseResult = await _client.post(
+      '/repos/KosukeSaigusa/spajam-2022/issues',
+      data: <String, dynamic>{
+        'title': title,
+        'body': body,
+      },
+    );
+    responseResult.when<void>(
+      success: (data) => logger.info('$data'),
+      failure: (message) => throw AppException(message: message),
+    );
+  }
+}
