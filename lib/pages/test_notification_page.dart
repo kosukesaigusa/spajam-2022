3e7da07e17b59619af1a11c25ae21b1ede9114bf
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-08-14 16:13:49 +0900

    chore: update test notification page

--- lib/pages/test_notification_page.dart
+++ lib/pages/test_notification_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../widgets/sign_in_required.dart';
 
 class TestNotificationPage extends StatelessWidget {
   const TestNotificationPage({super.key});
@@ -10,8 +13,11 @@ class TestNotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('通知テスト')),
-      body: const SizedBox(),
+      appBar: AppBar(title: const Text('テスト通知の受けページ')),
+      body: const EmptyPlaceholderWidget(
+        widget: FaIcon(FontAwesomeIcons.bell, size: 48),
+        message: 'このページはテスト通知の受けページです',
+      ),
     );
   }
 }
