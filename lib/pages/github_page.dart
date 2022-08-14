56a2b5d58f88b6a656989cf228526c8aff49cb89
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-15 01:08:39 +0900

    refactor: GitHubPage の継承元クラスを HookWidget に変更

--- lib/pages/github_page.dart
+++ lib/pages/github_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'github_issue_page.dart';
 import 'github_repos_page.dart';
 
-class GitHubPage extends HookConsumerWidget {
+class GitHubPage extends HookWidget {
   const GitHubPage({super.key});
 
   static const path = '/github';
@@ -13,7 +12,7 @@ class GitHubPage extends HookConsumerWidget {
   static const location = path;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final tabController = useTabController(initialLength: 2);
     return Scaffold(
       appBar: AppBar(
