24f09d210380745b20a370ee71486ce0d7a09497
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-20 19:25:38 +0900

    fix: voting event subscribe

--- lib/repositories/firestore/voting_event_repository.dart
+++ lib/repositories/firestore/voting_event_repository.dart
@@ -7,14 +7,15 @@ final votingEventRepositoryProvider =
     Provider<VotingEventRepository>((_) => VotingEventRepository());
 
 class VotingEventRepository {
-  /// List型ではあるが、１つのVotingEvent を返す
-  Stream<List<VotingEvent>> subscribeVotingEvent({required String roomId}) {
+  /// 最新の１つの VotingEvent を返す
+  /// Room が作成されたときに、必ず１つの VotingEvent が作成されている
+  Stream<VotingEvent> subscribeVotingEvent({required String roomId}) {
     final collectionStream = votingEventsRef(roomId: roomId)
         .orderBy('createdAt')
         .limit(1)
         .snapshots();
     return collectionStream.map(
-      (qs) => qs.docs.map((qds) => qds.data()).toList(),
+      (qs) => qs.docs.map((qds) => qds.data()).toList()[0],
     );
   }
 }
