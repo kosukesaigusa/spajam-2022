f774601fb9f4eb855454d8ef0ef65e90ad909b35
ryotaewamoto <lengtaiyanben@gmail.com>
2022-08-21 02:23:56 +0900

    fix: result page with subscribe voting event

--- lib/features/voting_event/voting_event.dart
+++ lib/features/voting_event/voting_event.dart
@@ -1,16 +1,35 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tuple/tuple.dart';
 
 import '../../models/voting_event.dart';
 import '../../repositories/firestore/voting_event_repository.dart';
 import '../../utils/exceptions/base.dart';
 import '../auth/auth.dart';
 
-/// VotingEvent 一覧を購読する StreamProvider。
+/// 最新の VotingEvent 一覧を購読する StreamProvider。
 final latestVotingEventStreamProvider =
     StreamProvider.family.autoDispose<VotingEvent, String>((ref, roomId) {
   final userId = ref.watch(userIdProvider).value;
   if (userId == null) {
     throw const AppException(message: 'サインインが必要です。');
   }
-  return ref.read(votingEventRepositoryProvider).subscribeLatestVotingEvent(roomId: roomId);
+  return ref
+      .read(votingEventRepositoryProvider)
+      .subscribeLatestVotingEvent(roomId: roomId);
+});
+
+/// 指定した VotingEvent 一覧を購読する StreamProvider。
+/// スコープが狭いので Tuple を使用する
+final votingEventStreamProvider = StreamProvider.family
+    .autoDispose<VotingEvent?, Tuple2<String, String>>((ref, tuple) {
+  final userId = ref.watch(userIdProvider).value;
+  if (userId == null) {
+    throw const AppException(message: 'サインインが必要です。');
+  }
+
+  final result = ref
+      .read(votingEventRepositoryProvider)
+      .subscribeVotingEvent(roomId: tuple.item1, votingEventId: tuple.item2);
+
+  return result;
 });
