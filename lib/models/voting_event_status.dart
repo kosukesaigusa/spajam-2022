702129af2c3fd9b71d88790b54b7a07cb6208080
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-21 02:45:00 +0900

    feat: VotingEventStatus に フィールドを追加

--- lib/models/voting_event_status.dart
+++ lib/models/voting_event_status.dart
@@ -1,6 +1,14 @@
 enum VotingEventStatus {
-  peace,
-  waiting,
-  voting,
-  finished;
+  peace(label: 'peace', mood: '😌'),
+  waiting(label: 'waiting', mood: '🥹'),
+  voting(label: 'voting', mood: '🗳'),
+  finished(label: 'finished', mood: '🎉');
+
+  const VotingEventStatus({
+    required this.label,
+    required this.mood,
+  });
+
+  final String label;
+  final String mood;
 }
