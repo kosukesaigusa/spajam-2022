d34e6a91eff83b4b913460aa16312bac9f22cdd1
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-20 19:54:03 +0900

    feat: show firestore data at room page

--- lib/pages/room_page.dart
+++ lib/pages/room_page.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../features/auth/auth.dart';
+import '../features/voting_event/voting_event.dart';
 import '../models/feeling.dart';
 import '../repositories/firestore/feeling_repository.dart';
 import '../utils/exceptions/base.dart';
+import '../utils/loading.dart';
 import '../utils/routing/app_router_state.dart';
 import '../utils/scaffold_messenger_service.dart';
-import '../utils/uuid.dart';
 import '../widgets/dialog.dart';
 import '../widgets/empty_placeholder.dart';
 
@@ -40,71 +41,83 @@ class RoomPage extends HookConsumerWidget {
     final userId = ref.watch(userIdProvider).value;
     final roomId = ref.watch(roomPageArgumentsProvider);
 
-    if (userId == null) {
+    Widget baseScaffold(Widget body, {Widget? floatingActionButton}) {
       return Scaffold(
         appBar: AppBar(title: const Text('ルーム')),
-        body: const EmptyPlaceholderWidget(
+        floatingActionButton: floatingActionButton,
+        body: body,
+      );
+    }
+
+    if (userId == null) {
+      return baseScaffold(
+        const EmptyPlaceholderWidget(
           message: 'サインインが必要です。',
         ),
       );
     }
-    return Scaffold(
-      appBar: AppBar(title: const Text('ルーム')),
-      body: Column(
-        children: [
-          Text('roomId: $roomId'),
-          const Text('あなたは　不快　を選択しています。'),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // 後でお手製
-          final isComfortable = await ref
-              .read(scaffoldMessengerServiceProvider)
-              .showDialogByBuilder<bool>(
-                builder: (context) => AlertDialog(
-                  content: CommonAlertDialogContent(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        // コンテンツ領域
-                        SimpleDialogOption(
-                          onPressed: () => Navigator.pop(context, true),
-                          child: const Text('快適'),
-                        ),
-                        SimpleDialogOption(
-                          onPressed: () => Navigator.pop(context, false),
-                          child: const Text('不快'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
 
-          if (isComfortable == null) {
-            return;
-          }
-          try {
-            await ref.read(feelingRepositoryProvider).setFeeling(
-                  roomId: 'gxdmunvBcT5noyAP5FbJ',
-                  votingEventId: 'pa6zlcjqr0B2jx6rL9Tg',
-                  feeling: Feeling(
-                    feelingId: uuid,
-                    userId: userId,
-                    isComfortable: isComfortable,
-                  ),
-                );
-          } on Exception catch (e) {
-            ref
-                .read(scaffoldMessengerServiceProvider)
-                .showSnackBarByException(e);
-          }
-        },
-        child: const Icon(
-          Icons.message,
-        ),
-      ),
-    );
+    return ref.watch(votingEventStreamProvider(roomId)).when(
+          data: (votingEvent) => Center(
+            child: baseScaffold(
+              Text(votingEvent.toString()),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () async {
+                  // 後でお手製
+                  final isComfortable = await ref
+                      .read(scaffoldMessengerServiceProvider)
+                      .showDialogByBuilder<bool>(
+                        builder: (context) => AlertDialog(
+                          content: CommonAlertDialogContent(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                // コンテンツ領域
+                                SimpleDialogOption(
+                                  onPressed: () => Navigator.pop(context, true),
+                                  child: const Text('快適'),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () =>
+                                      Navigator.pop(context, false),
+                                  child: const Text('不快'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+
+                  if (isComfortable == null) {
+                    return;
+                  }
+                  try {
+                    await ref.read(feelingRepositoryProvider).setFeeling(
+                          roomId: roomId,
+                          votingEventId: votingEvent.votingEventId,
+                          userId: userId,
+                          feeling: Feeling(
+                            userId: userId,
+                            isComfortable: isComfortable,
+                          ),
+                        );
+                  } on Exception catch (e) {
+                    ref
+                        .read(scaffoldMessengerServiceProvider)
+                        .showSnackBarByException(e);
+                  }
+                },
+                child: const Icon(
+                  Icons.message,
+                ),
+              ),
+            ),
+          ),
+          error: (e, _) => Center(
+            child: baseScaffold(Text(e.toString())),
+          ),
+          loading: () =>
+              baseScaffold(const Center(child: PrimarySpinkitCircle())),
+        );
   }
 }
