c23de400998dc27e0a0f5c2197d4b5d3db890ad8
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-20 16:16:13 +0900

    Rooms

---
+++ lib/models/rooms.dart
@@ -0,0 +1,15 @@
+// import 'package:freezed_annotation/freezed_annotation.dart';
+
+// part 'rooms.freezed.dart';
+// part 'rooms.g.dart';
+
+// @freezed
+// class Rooms with _$Rooms {
+//   const factory Rooms({
+//     @Default('') String roomId,
+//     @Default('') String roomName,
+//     @Default(<String>[]) List<String> userIds,
+//   }) = _Rooms;
+
+//   factory Rooms.fromJson(Map<String, dynamic> json) => _$RoomsFromJson(json);
+// }
