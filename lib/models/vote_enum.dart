3e75a0aa556810c13a642e25cc377509a03d62b9
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-20 17:23:40 +0900

    feat: add voteEnum label

--- lib/models/vote_enum.dart
+++ lib/models/vote_enum.dart
@@ -1,7 +1,12 @@
 enum VoteEnum {
-  extremelyHot,
-  hot,
-  comfortable,
-  cold,
-  extremelyCold;
+  extremelyHot('めちゃ暑い'),
+  hot('暑い'),
+  comfortable('快適'),
+  cold('寒い'),
+  extremelyCold('めちゃ寒い');
+
+  const VoteEnum(this.label);
+
+  /// 表示ラベル。
+  final String label;
 }
