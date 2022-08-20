b5a442d67d187ad0a6f78e6f35dd5db7f237e988
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-20 17:59:12 +0900

    feat: create RoomsPage

--- lib/pages/rooms_page.dart
+++ lib/pages/rooms_page.dart
@@ -3,9 +3,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../features/auth/auth.dart';
-import '../features/todo/todo.dart';
+import '../features/room/room.dart';
 import '../utils/loading.dart';
 import '../widgets/empty_placeholder.dart';
+import 'room_page.dart';
 
 /// ルーム一覧ページ
 class RoomsPage extends HookConsumerWidget {
@@ -25,26 +26,36 @@ class RoomsPage extends HookConsumerWidget {
               message: '（仮）ルーム一覧を表示するにはサインインが必要です。'
                   '「About > 匿名サインイン」から匿名でのサインインを行ってください。',
             )
-          : ref.watch(todosStreamProvider).when(
-                data: (todos) {
-                  if (todos.isEmpty) {
-                    return EmptyPlaceholderWidget(
-                      widget: const FaIcon(
+          : ref.watch(roomsStreamProvider).when(
+                data: (rooms) {
+                  if (rooms.isEmpty) {
+                    return const EmptyPlaceholderWidget(
+                      widget: FaIcon(
                         FontAwesomeIcons.list,
                         color: Colors.black45,
                         size: 48,
                       ),
-                      message: '${ref.watch(todoFilterProvider).emptyMessage}TODO がありません。'
-                          '右下のペンボタンから、TODO を登録してください。',
+                      message: 'Room がありません。'
+                          'Room を作成してください。',
                     );
                   }
                   return ListView.separated(
                     separatorBuilder: (context, index) => const Divider(),
-                    itemCount: todos.length,
+                    itemCount: rooms.length,
                     padding: const EdgeInsets.all(16),
                     // TODO: 仮で SizedBox() にしています。
                     // itemBuilder: (context, index) => RoomItem(todo: todos[index]),
-                    itemBuilder: (context, index) => const SizedBox(),
+                    itemBuilder: (context, index) {
+                      final room = rooms[index];
+                      return ListTile(
+                        title: Text(room.roomName),
+                        onTap: () => Navigator.pushNamed<void>(
+                          context,
+                          RoomPage.location,
+                          arguments: room,
+                        ),
+                      );
+                    },
                   );
                 },
                 error: (_, __) => const SizedBox(),
