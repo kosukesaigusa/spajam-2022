17e8cfb5d6b4f01b3606180ea26907bd9bcc4c1e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-20 23:11:33 +0900

    feat: result page ui

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,21 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../features/voting_event/voting_event.dart';
+import '../models/voting_event_status.dart';
+import '../utils/exceptions/base.dart';
+import '../utils/loading.dart';
+import '../utils/routing/app_router_state.dart';
+
+/// roomId を取得してから返す Provider。
+final _roomIdProvider = Provider.autoDispose<String>(
+  (ref) {
+    try {
+      final state = ref.read(appRouterStateProvider);
+      final roomId = state.params['roomId']!;
+      return roomId;
+    } on Exception {
+      throw const AppException(message: 'ルームが見つかりませんでした。');
+    }
+  },
+  dependencies: <ProviderOrFamily>[
+    appRouterStateProvider,
+  ],
+);
+
 /// 投票結果ページ。
 /// 対象の VotingEvent.status が voting or finished のときに表示する想定。
 class ResultPage extends HookConsumerWidget {
   const ResultPage({super.key});
 
-  static const path = '/result';
+  static const path = '/rooms/:roomId/:votingEventId/result';
   static const name = 'ResultPage';
-  static const location = path;
-
+  static String location({
+    required String roomId,
+    required String votingEventId,
+  }) =>
+      '/rooms/$roomId/$votingEventId/result';
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('結果')),
-      body: const SizedBox(),
-    );
+    final roomId = ref.watch(_roomIdProvider);
+    return ref.watch(votingEventStreamProvider(roomId)).when(
+          data: (voteEvent) {
+            switch (voteEvent.status) {
+              case VotingEventStatus.voting:
+                return const VotingWidget();
+              case VotingEventStatus.finished:
+                return const FinishedWidget();
+              // ignore: no_default_cases
+              default:
+                return Scaffold(
+                  appBar: AppBar(title: const Text('エラー')),
+                  body: const Center(
+                    child: Text('投票結果ページには投票中または終了済みのイベントに対してのみ表示できます。'),
+                  ),
+                );
+            }
+          },
+          error: (error, stackTrace) => Scaffold(
+            appBar: AppBar(title: const Text('投票結果')),
+            body: Center(
+              child: Text(error.toString()),
+            ),
+          ),
+          loading: () => Scaffold(
+            appBar: AppBar(title: const Text('')),
+            body: const Center(
+              child: PrimarySpinkitCircle(),
+            ),
+          ),
+        );
   }
 }
 
@@ -25,7 +77,7 @@ class VotingWidget extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const SizedBox();
+    return const Scaffold(body: Text('投票中'));
   }
 }
 
@@ -35,6 +87,6 @@ class FinishedWidget extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const SizedBox();
+    return const Scaffold(body: Text('投票終了'));
   }
 }
